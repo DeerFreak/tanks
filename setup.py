4dd0373bbe5f451dabd2cec4517123a6295c057a
Jakob Kruse <jakob.kruse@gmx.de>
2018-08-19 16:53:36 +0200

    Initialized setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='Tanks',
+      packages=find_packages(),
+      install_requires=[
+          'nose',
+          'coverage',
+          'numpy',
+          'pygame'
+
+      ])
