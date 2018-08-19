50478fbf9c55a0d239924c77ff6c04a8cb2ac641
jakob <jakob@winter-wind.de>
2018-08-19 19:20:49 +0200

    ganz viel neues

--- init.py
+++ init.py
@@ -1,4 +1,15 @@
 import pygame
+from colors import *
+import tank_class
 
-def initialize_game():
-    pass
+def initialize_game(resolution):
+    surf = pygame.display.set_mode((resolution[0], resolution[1]), pygame.FULLSCREEN)
+    pygame.display.fill(WHITE)
+    pygame.display.update()
+    tanks = []
+    for i in [(200, 500), (800, 600)]:
+        tanks.append(tank(i, surf))
+    return (surf, tanks)
+
+
+
