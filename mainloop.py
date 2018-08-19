6b60ea2084723bb96f7489efc3287a3b4b61e6ab
Jakob Kruse <jakob.kruse@gmx.de>
2018-08-19 21:24:24 +0200

    First program

--- mainloop.py
+++ mainloop.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import time
+from colors import *
 from pygame.locals import *
 
 
@@ -14,6 +15,35 @@ def mainloop(game):
     while Running:
         event_dict = events(event_dict)
 
+        if event_dict["W"] == True:
+            tank1.moving = 1
+        elif event_dict["S"] == True:
+            tank1.moving = -1
+        elif event_dict["W"] == False and event_dict["S"] == False:
+            tank1.moving = 0
+        if event_dict["A"] == True:
+            tank1.angle -= tank1.turn_speed
+        if event_dict["D"] == True:
+            tank1.angle += tank1.turn_speed
+
+        if event_dict["PO"] == True:
+            tank2.moving = 1
+        elif event_dict["PU"] == True:
+            tank2.moving = -1
+        elif event_dict["PO"] == False and event_dict["PU"] == False:
+            tank2.moving = 0
+        if event_dict["PL"] == True:
+            tank2.angle -= tank2.turn_speed
+        if event_dict["PR"] == True:
+            tank2.angle += tank2.turn_speed
+
+        tank1.move()
+        tank2.move()
+
+        surf.fill(WHITE)
+        tank1.plot()
+        tank2.plot()
+        pygame.display.update()
 
 
 def events(event_dict):
@@ -45,6 +75,10 @@ def events(event_dict):
                 event_dict["Space"] = True
             if event.key == K_p:
                 event_dict["P"] = True
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            
 
         if event.type == KEYUP:
             if event.key == K_UP:
