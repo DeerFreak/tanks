d03d74fa2cc3444d25fb1811ddb8bd40885e54a4
Jakob Kruse <jakob.kruse@gmx.de>
2018-08-19 20:59:45 +0200

    added key bindings

--- mainloop.py
+++ mainloop.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 from pygame.locals import *
 
 
@@ -9,15 +10,15 @@ def mainloop(game):
     tank2 = game[1][1]
     Running = True
     event_dict = {"W": False, "A": False, "S": False, "D": False, "Space": False,
-                  "PO": False, "PL": False, "PU": False, "PR": False, "UE": False}
+                  "PO": False, "PL": False, "PU": False, "PR": False, "P": False}
     while Running:
-        pass
+        event_dict = events(event_dict)
 
 
 
 def events(event_dict):
     # Panzer1: WASD und Space
-    # Panzer2: Pfeiltasten und Ü
+    # Panzer2: Pfeiltasten und P
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -40,17 +41,44 @@ def events(event_dict):
                 event_dict["S"] = True
             if event.key == K_d:
                 event_dict["D"] = True
+            if event.key == K_SPACE:
+                event_dict["Space"] = True
+            if event.key == K_p:
+                event_dict["P"] = True
+
+        if event.type == KEYUP:
+            if event.key == K_UP:
+                event_dict["PO"] = False
+            if event.key == K_DOWN:
+                event_dict["PU"] = False
+            if event.key == K_LEFT:
+                event_dict["PL"] = False
+            if event.key == K_RIGHT:
+                event_dict["PR"] = False
+            if event.key == K_w:
+                event_dict["W"] = False
+            if event.key == K_a:
+                event_dict["A"] = False
+            if event.key == K_s:
+                event_dict["S"] = False
+            if event.key == K_d:
+                event_dict["D"] = False
+            if event.key == K_SPACE:
+                event_dict["Space"] = False
+            if event.key == K_p:
+                event_dict["P"] = False
 
     return event_dict
 
 if __name__ == "__main__":
     while True:
-        pygame.init()
         pygame.display.set_mode((200,200))
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_w:
-                    print("W gedrückt")
+                    print("w")
             if event.type == KEYUP:
                 if event.key == K_w:
-                    print("W losgelassen")
+                    print("!w")
+
+
